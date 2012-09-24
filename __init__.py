49efa6b8a1d6949e1e30cdc25aadd7688b01c538
Matthew Webb <matthew@stratus.Belkin>
2012-09-23 21:28:51 -0600

    Making famarc into a pyramid web app.
    Signed-off-by: Matthew Webb <matthew@stratus.Belkin>

--- __init__.py
+++ __init__.py
@@ -1,18 +1,21 @@
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+
+from .models import DBSession
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    config = Configurator(settings=settings)
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.scan()
+    return config.make_wsgi_app()
+
 settings = {
 
     'file_dir'  : '/Users/matthew/pylib/famarc/files',
     'db_file'   : '/Users/matthew/pylib/famarc/db/data.db',
 }
-
-#sqlalchemy init
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///{}'.format(settings['db_file']), echo=True)
-Base = declarative_base(engine)
-Session = sessionmaker(bind=engine)
-
-#package init
-import utils
-import models
-import upload
